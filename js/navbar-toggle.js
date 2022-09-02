454077e12a82ce5bfab45322df7bc4bb838edce5
Chika-lab <71614037+Chika-lab@users.noreply.github.com>
2022-09-03 01:38:12 +0900

    Delete navbar-toggle.js

--- js/navbar-toggle.js
+++
@@ -1,7 +0,0 @@
-// navbar-toggle の処理
-$(function() {
- $('.navbar-toggle').click(function () {
-  $('.header_sp').toggleClass('active');
-  $('.header_sp .header-dropdown').slideToggle();
- });
-});
