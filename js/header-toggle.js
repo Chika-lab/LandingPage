279da0ad82f30d86d380aa6355285d56d2d85045
Chika-lab <71614037+Chika-lab@users.noreply.github.com>
2022-09-13 22:54:00 +0900

    Update header-toggle.js

--- js/header-toggle.js
+++ js/header-toggle.js
@@ -1,5 +1,6 @@
 $(function() {
   $('.navbar-toggle').on('click', function() {
+    $('.header_pc').toggleClass('active');
     $('.header_sp').toggleClass('active');
     $(".header-dropdown").toggle(100);
   });
