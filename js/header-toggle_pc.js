05fa7c51ee08d12f73c4438d4d67135aae564e01
eiganken <eiganken.delvelop@gmail.com>
2022-09-14 01:26:26 +0900

    Update header-toggle_pc.jp

--- js/header-toggle_pc.js
+++ js/header-toggle_pc.js
@@ -3,4 +3,9 @@ $(function() {
     $('.header_pc').toggleClass('active');
     $(".header-dropdown_pc").toggle(100);
   });
+
+  $('.header-dropdown_pc > li').on('click', function() {
+    $('.header_pc').removeClass('active');
+    $(".header-dropdown_pc").hide(200);
+  })
 });
